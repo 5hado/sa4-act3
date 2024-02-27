f71ad16819f75ee387d2c4f5ff537c34a0379a39
Kevin Gibson <ninjamaster0822@gmail.com>
2024-02-27 17:35:31 -0600

    User now get as many chances as the want and can quit by typing q

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,18 @@
-number = 10
+import random
+
+number = random.randint(1, 10)
+
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while(True):
+    guess = input("What number am I thinking of? ")
+
+    if guess == "q":
+        break
+    guess = int(guess)
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+       
+    else:
+        print("Wrong")
+    
+print(f"Sorry! The number was {number}.")
