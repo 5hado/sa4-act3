1c6badb871e383a6fa2c5505b6d1c9803d46670e
Kevin Gibson <ninjamaster0822@gmail.com>
2024-02-27 17:44:06 -0600

    The user now has 5 tries to guess or they lose

--- guess_number.py
+++ guess_number.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(1, 10)
 
 print("I'm thinking of a number...")
-while(True):
+for u in range(5):
     guess = input("What number am I thinking of? ")
 
-    if guess == "q":
+    if guess == "q" or u == 4 :
         print(f"Sorry! The number was {number}.")
         break
     guess = int(guess)
@@ -18,4 +18,6 @@ while(True):
         print("Wrong your guess is too high")
     else:
         print("Wrong you guess is too low")
+    
+    print("You have " + str(4-u) + " more tries")
     
